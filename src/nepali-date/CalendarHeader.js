3e9fa7a326ea501f21003ae3965ceacf1c994785
Damodar Lohani <dlohani48@gmail.com>
2017-12-25 06:19:14 +0545

    header view prop types defined

--- src/nepali-date/CalendarHeader.js
+++ src/nepali-date/CalendarHeader.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import DevnagariDates from './DevnagariDates';
 
 const CalendarHeaderView = ({month, year, ...props}) => (
     <div className="calendar-header-view">
-       <button onClick={props.onClickPrevious} className="left"> &lt; </button> {month} {year} <button className="right" onClick={props.onClickNext}> &gt; </button>
+       <button onClick={props.onClickPrevious} className="left"> &lt; </button> {DevnagariDates.getNeMonthName(month)} {DevnagariDates.getNeYear(year)} <button className="right" onClick={props.onClickNext}> &gt; </button>
     </div>
 )
+
+CalendarHeaderView.propTypes = {
+    onClickPrevious: PropTypes.func.isRequired,
+    onClickNext: PropTypes.func.isRequired,
+    month: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired
+}
 
 export default CalendarHeaderView;
